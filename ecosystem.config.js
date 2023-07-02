2f9e9c34fc4447f9c8407a576e4bf27ac141efa0
Jiwon <winning1993@naver.com>
2023-07-02 20:23:55 +0900

    [feat] 콜리세우스 서버 클러스터링 테스트중

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,10 @@ const os = require('os');
 
 module.exports = {
     apps: [{
-        port: 5002,
+        port: 5001,
         name: "colyseus",
         script: "dist/server.js",
         watch: true,
-        instance_var: 'INSTANCE_ID',
         // instances: os.cpus().length,
         instances: 2,
         exec_mode: 'fork',
--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,20 @@ import { Server as ColyseusServer, LobbyRoom } from "colyseus";
 import { RedisDriver } from "@colyseus/redis-driver";
 import { RedisPresence } from "@colyseus/redis-presence";
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
-
+import dotenv from 'dotenv';
 // Routes
 import { index } from "./routes/index";
 import { createServer } from "http";
 import { MainRoom } from "./multiplay/rooms/MainRoom";
 import cors from "cors";
 
-
 // Create Express server
 export const app = express();
 
+dotenv.config();
+
 // Express configuration
-app.set("port", process.env.PORT || 5001);
+app.set("port", + Number(process.env.NODE_APP_INSTANCE) + Number(process.env.PORT));
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 
@@ -26,6 +27,8 @@ app.use(logger("dev"));
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", index);
 app.use(cors());
+
+
 
 
 app.use(errorNotFoundHandler);
@@ -37,11 +40,13 @@ const gameServer = new ColyseusServer({
     presence: new RedisPresence(),
     driver: new RedisDriver(),
 
-    
 });
-console.log(process.env.INSTANCE_ID);
-console.log('\u001b[31m', process.env.INSTANCE_ID);
-gameServer.listen(parseInt(process.env.PORT) + Number(process.env.INSTANCE_ID) || 5002).then(data => {
+console.log(process.env);
+console.log(process.env.PORT)
+console.log(process.env.NODE_ENV)
+console.log('\u001b[31m', process.env.NODE_APP_INSTANCE);
+console
+gameServer.listen(Number(process.env.PORT) + 1 + Number(process.env.NODE_APP_INSTANCE)).then(data => {
     console.log(data);
 });
 
